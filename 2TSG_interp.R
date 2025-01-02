36c1150e92699bf43c5cf535a9a0079db327efde
Diana <cardosodiana@yahoo.ca>
2025-01-02 15:01:12 -0400

    Update 2TSG_interp.R
    change var name from 2code_interp_plot_TSGdatapath to code_interp_plot_TSGdatapath

--- 2TSG_interp.R
+++ 2TSG_interp.R
@@ -79,9 +79,9 @@ if(!dir.exists(file.path(pathprocessed))) {
 }
 
 # if directories \2code_interp_plot_TSGdata path doesn't exist create it
-2code_interp_plot_TSGdatapath <- file.path(source_code_directory, "2code_interp_plot_TSGdata")
-if(!dir.exists(file.path(2code_interp_plot_TSGdatapath))) {
-    dir.create(file.path(2code_interp_plot_TSGdatapath))
+code_interp_plot_TSGdatapath <- file.path(source_code_directory, "2code_interp_plot_TSGdata")
+if(!dir.exists(file.path(code_interp_plot_TSGdatapath))) {
+    dir.create(file.path(code_interp_plot_TSGdatapath))
 }
 
 # Create the directory where intermediate hourly plots and data will be stored 
